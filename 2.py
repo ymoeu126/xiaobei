435e86bd42f75307134e068f04e73fe5323c05d7
ymoeu126 <78584425+ymoeu126@users.noreply.github.com>
2021-02-07 17:17:13 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,14 +4,13 @@ import requests
 import base64
 import os
 import json
+import sys
 
-#配置账号
 username= os.getenv("XIAOBEI_USERNAME")
 pd= os.getenv("XIAOBEI_PASSWORD")
 password=base64.b64encode(pd.encode())
 password=password.decode()
 SCKEY = os.getenv("XIAOBEI_SCKEY")
-
 #健康打卡提交的信息
 temperature={
     "temperature": "36.3",
@@ -38,40 +37,74 @@ headers = {
 #一系列的url
 login_url ='https://xiaobei.yinghuaonline.com/prod-api/login'
 health_url='https://xiaobei.yinghuaonline.com/prod-api/student/health'
-yanzhengma_url='https://xiaobei.yinghuaonline.com/prod-api/captchaImage'
+captchaImage_url='https://xiaobei.yinghuaonline.com/prod-api/captchaImage'
 
 session = requests.Session()
-#获取滑动验证码的code和uuid传给post_data进行登录验证
-q1=session.get(yanzhengma_url,headers=headers )
-code=q1.json()['showCode']
-uuid=q1.json()['uuid']
+def captchaImage_session(_captchaImage_url):
+    #获取滑动验证码的code和uuid传给post_data进行登录验证
+    _captchaImage_session=session.get(captchaImage_url,headers=headers,verify=False )
+    return json.loads(_captchaImage_session.text)
+def login_session(_captchaImage_session,_username,_password):
+    code=_captchaImage_session['showCode']
+    uuid=_captchaImage_session['uuid']
+    post_data={
+        "username":username,
+        "password":password,
+        "code":code,
+        "uuid":uuid
+    }
 
-post_data={
-    "username":username,
-    "password":password,
-    "code":code,
-    "uuid":uuid
-}
+    #登录小北同学得到token并且传给headers中的Authorization发送请求头中
+    _login_session=session.post(login_url,json = post_data,headers = headers,verify=False)
+    return json.loads(_login_session.text)
 
-#登录小北同学得到token并且传给headers中的Authorization发送请求头中
-q2=session.post(login_url,json = post_data,headers = headers)
-token=q2.json()['token']
-headers["Authorization"] ='Bearer ' +token
+def get_token(_login_session):
+    try:
+        token=_login_session['token']
+        headers["Authorization"] ='Bearer ' +token
+        post_health()
+    except Exception as e:
+        print('账号或者密码错误')
 
+def post_health():
+    _post_health=session.post(health_url,json = temperature,headers = headers,verify=False)
+    return json.loads(_post_health.text)
 
-#提交健康打卡信息
-response2 = session.post(health_url,json = temperature,headers = headers)
-if response2.status_code == 200:
-    message='打卡成功'   
-else:
-    message='请仔细阅读步骤,或者出现了bug'
-print(message)    
-#server酱
-api = "https://sc.ftqq.com/"+SCKEY+".send"
-server_data = {
-    "text": '小北同学打卡',
-    "desp": message
-}
-requests.post(api, data = server_data)
+ 
+def notify(_title, _message=None):
+    if not _message:
+        _message = _title
 
+    print(_title)
+
+    _response = requests.post(f"https://sc.ftqq.com/{SCKEY}.send", {"text": _title, "desp": _message},verify=False)
+
+    if _response.status_code == 200:
+        print(f"发送通知状态：{_response.content.decode('utf-8')}")
+    else:
+        print(f"小北同学打卡成功，但未成功配置server酱,问题不大：{_response.status_code}")
+
+if __name__ == "__main__":
+    if not username or not password:
+        notify("用户名或账号为空，请仔细阅读配置步骤！")
+        sys.exit()
+    _captchaImage_session=captchaImage_session(captchaImage_url)
+    _login_session=login_session(_captchaImage_session,username,password)
+    get_token(_login_session)
+    try:
+        response=post_health()
+        if response['code'] == 200:
+            notify("小北同学打卡成功")
+        else:
+            notify("打卡失败，请手动打卡", response.text)
+    except Exception as e:
+        notify("打卡失败，请手动打卡", str(e))
+
+    
+    
+    
+    
+    
+    
+    
 
